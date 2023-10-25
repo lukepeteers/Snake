40a04b8631ec0721b0840bf0b207bdb66ea2ff45
Hortybean <tyler.metcalfe96@gmail.com>
2023-10-25 12:49:41 +0100

    added movement

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector2 _direction = Vector2.right;
 
-    // Update is called once per frame
-    void Update()
+   private void Update()
+
     {
-        
-    }
-}
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S)){
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A)) {
+            _direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D)){
+            _direction = Vector2.right;
+        }
+        }
+
+
+    private void FixedUpdate() => this.transform.position = new Vector3(
+               Mathf.Round(this.transform.position.x) + _direction.x,
+               Mathf.Round(this.transform.position.y) + _direction.y,
+               0.0f);
+
+
+
+
+
+} 
+
